98eddcdeb784e475d4ce916344946589144e1397
francomonja <51922845+francomonja@users.noreply.github.com>
2023-05-12 13:54:07 -0300

    cambio de storage

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,14 @@
 
 export const environment = {
   firebase: {
-  apiKey: "AIzaSyCFtoFVAqC32apv6jjDG9UQskF99WWdRKU",
-  authDomain: "portfoliomonja.firebaseapp.com",
-  projectId: "portfoliomonja",
-  storageBucket: "portfoliomonja.appspot.com",
-  messagingSenderId: "688733561088",
-  appId: "1:688733561088:web:e24769ffe6f6bc3f15d3e5",
-  measurementId: "G-57X536M8CF",
-  locationId: 'nam5 (us-central)',
+    projectId: 'portfoliomonja',
+    appId: '1:688733561088:web:e24769ffe6f6bc3f15d3e5',
+    storageBucket: 'portfoliomonja.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCFtoFVAqC32apv6jjDG9UQskF99WWdRKU',
+    authDomain: 'portfoliomonja.firebaseapp.com',
+    messagingSenderId: '688733561088',
+    measurementId: 'G-57X536M8CF',
   },
   production: false
 };
