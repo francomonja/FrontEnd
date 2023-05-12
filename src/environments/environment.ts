dca1f97cc29140f6eb34b75de3d9d7658e8aae9d
francomonja <51922845+francomonja@users.noreply.github.com>
2023-05-11 21:26:18 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,14 @@
 
 export const environment = {
   firebase: {
-    projectId: 'portfolio-173b4',
-    appId: '1:470397938960:web:7f4a7fcd2b517f8be3c995',
-    storageBucket: 'portfolio-173b4.appspot.com',
-    locationId: 'southamerica-east1',
-    apiKey: 'AIzaSyBcv1XMbdXRoFIRRp8LbnEpuHpLQ-pZhmY',
-    authDomain: 'portfolio-173b4.firebaseapp.com',
-    messagingSenderId: '470397938960',
-    measurementId: 'G-SNN2SF4CDP',
+  apiKey: "AIzaSyCFtoFVAqC32apv6jjDG9UQskF99WWdRKU",
+  authDomain: "portfoliomonja.firebaseapp.com",
+  projectId: "portfoliomonja",
+  storageBucket: "portfoliomonja.appspot.com",
+  messagingSenderId: "688733561088",
+  appId: "1:688733561088:web:e24769ffe6f6bc3f15d3e5",
+  measurementId: "G-57X536M8CF",
+  locationId: 'southamerica-east1',
   },
   production: false
 };
